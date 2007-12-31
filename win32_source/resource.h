8771fa536c61437a8a583e5b37f5f07a8635dff6
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2007-12-31 03:59:49 +0000

    And... an exit button. :P

--- win32_source/resource.h
+++ win32_source/resource.h
@@ -9,13 +9,14 @@
 #define IDM_HELP                        40004
 #define IDM_ENGLISH                     40005
 #define IDM_MYANMAR                     40006
+#define IDM_EXIT                        40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
-#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
