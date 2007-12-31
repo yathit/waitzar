c82ba179930eec5105a9a54bd4f23e1e2a74cd40
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2007-12-31 03:18:26 +0000

    Main menu added.

--- win32_source/resource.h
+++ win32_source/resource.h
@@ -4,15 +4,18 @@
 //
 #define ICON_WZ                         101
 #define ICON_WZ_ENG                     102
-#define IDI_ICON1                       103
 #define ICON_WZ_MM                      103
+#define IDR_MENU1                       104
+#define WZ_MENU                         104
+#define ID_HELP_ENGLISH                 40001
+#define ID_HELP_ENGLISH40002            40002
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40003
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
