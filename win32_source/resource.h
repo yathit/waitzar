60277d2c14a1c5f417d43a83ad6c5db484348464
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2007-12-31 06:56:18 +0000

    Got the model file to load as an embedded resource.

--- win32_source/resource.h
+++ win32_source/resource.h
@@ -6,6 +6,8 @@
 #define ICON_WZ_ENG                     102
 #define ICON_WZ_MM                      103
 #define WZ_MENU                         104
+#define IDR_MODEL1                      105
+#define WZ_MODEL                        105
 #define IDM_HELP                        40004
 #define IDM_ENGLISH                     40005
 #define IDM_MYANMAR                     40006
@@ -15,7 +17,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
