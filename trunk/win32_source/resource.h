a84e9de46bcf841bacdf6f63f0d022e04964d0c7
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2008-04-21 09:46:35 +0000

    Changed menu message & help message dynamically.

--- trunk/win32_source/resource.h
+++ trunk/win32_source/resource.h
@@ -8,19 +8,18 @@
 #define WZ_MENU                         104
 #define WZ_MODEL                        105
 #define ICON_WZ_LOADING                 106
-#define IDR_COREFONT1                   107
 #define WZ_FONT                         107
 #define IDM_HELP                        40004
-#define IDM_ENGLISH                     40005
 #define IDM_MYANMAR                     40006
 #define IDM_EXIT                        40008
+#define IDM_ENGLISH                     40010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
-#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_COMMAND_VALUE         40011
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
