6bdbb4cb832a53743a9670d32add18138762c667
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2008-05-14 19:28:30 +0000

    Window starts in the right spot. Also, added a smaller font.

--- trunk/win32_source/resource.h
+++ trunk/win32_source/resource.h
@@ -9,6 +9,8 @@
 #define WZ_MODEL                        105
 #define ICON_WZ_LOADING                 106
 #define WZ_FONT                         107
+#define IDR_COREFONT1                   108
+#define WZ_SMALL_FONT                   108
 #define IDM_HELP                        40004
 #define IDM_MYANMAR                     40006
 #define IDM_EXIT                        40008
@@ -18,7 +20,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40011
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
