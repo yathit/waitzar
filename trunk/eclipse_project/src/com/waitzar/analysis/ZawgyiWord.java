e7fea5979f712a1763da31f76511698aea071e98
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2008-06-05 09:24:16 +0000

    Minor fix....

--- trunk/eclipse_project/src/com/waitzar/analysis/ZawgyiWord.java
+++ trunk/eclipse_project/src/com/waitzar/analysis/ZawgyiWord.java
@@ -9,19 +9,19 @@ import java.util.regex.Pattern;
  *  1) Many zero-width characters in a row (1039+1039) might be illegal and difficult to detect visually.
  *  2) When a sequence of zero-width characters IS valid, a strict ordering is imposed to prevent
  *     having one sequence treated as two distinct ones.
- *  3) Zawgyi illegally disunifies several characters. Even getting past that, Burmese with no dialects 
- *     disunifies short/long "ar". This class can detect such collisions, and deal with them 
- *     when necessary (e.g., for sorting). 
+ *  3) Zawgyi illegally disunifies several characters. Even getting past that, Burmese with no dialects
+ *     disunifies short/long "ar". This class can detect such collisions, and deal with them
+ *     when necessary (e.g., for sorting).
  * @author Seth N. Hetu
  */
 public class ZawgyiWord {
 	//Regexes
 	public static Pattern consonantRegex = Pattern.compile("([\\u1000-\\u1021[\\u108F[\\u104C-\\u104F]]])[^\\u1039]");
 	public static Pattern medialRegex = Pattern.compile("((\\-\\u103A)|(\\u103B\\-)|(\\u107E\\-)|(\\-\\u103C)|(\\-\\u103D)|(\\-\\u103C\\u103A)|(\\-\\u103D\\u103A)|(\\u1081\\-\\u103D)|(\\u1082\\-\\u103D)|(\\-\\u108A)|(\\u1081\\-\\u108A)|(\\u1082\\-\\u108A))");
-	
+
 	//The actual string the user entered
 	private String rawText;
-	
+
 	//Sorting elements
 	private String sortConsonant = "";
 	private String sortMedial = "";
@@ -29,42 +29,38 @@ public class ZawgyiWord {
 	private String sortVowel = "";
 	private String sortTone = "";
 	private String sortUnknown = "";
-	
+
 	public ZawgyiWord(String zawgyiText) {
 		//Before anything, re-order the string where necessary.
 		ZawgyiWord.properlyOrder(zawgyiText);
-		
-		//Even invalid sequences still have representable text. 
+
+		//Even invalid sequences still have representable text.
 		this.rawText = zawgyiText;
-		
+
 		//Errors?
 		if (ZawgyiWord.isInError(zawgyiText))
 			return;
-		
+
 		//Handle independent vowels and special contractions
-		if (this.rawText.equals("\u1023"))
-			segmentWord("\u1021\u102D");
-		else if (this.rawText.equals("\u1024"))
-			segmentWord("\u1021\u102E");
-		else if (this.rawText.equals("\u1025"))
-			segmentWord("\u1021\u102F");
-		else if (this.rawText.equals("\u1026") || this.rawText.equals("\u1025\u102E"))
-			segmentWord("\u1021\u1030");
-		else if (this.rawText.equals("\u1027"))
-			segmentWord("\u1021\u1031");
-		else if (this.rawText.equals("\u1029") || this.rawText.equals("\u107E\u101E"))
-			segmentWord("\u1031\u1021\u102C");
-		else if (this.rawText.equals("\u102A") || this.rawText.equals("\u1031\u107E\u101E\u102C\u1039"))
-			segmentWord("\u1031\u1021\u102C\u1039");
-		else if (this.rawText.equals("\u1031\u101A\u102C\u1000\u1039\u103A\u102C\u1038"))
-			segmentWord("\u101A\u102C\u1000\u1039\u1000\u103A\u102C\u1038");
-		else if (this.rawText.equals("\u1000\u103C\u103A\u108F\u102F\u1039\u1015\u1039"))
-			segmentWord("\u1000\u103C\u103A\u108F\u102F\u1014\u102F\u1015\u1039");
-		else
-			segmentWord(this.rawText);
+		String[] independentVowels = new String[] {"\u1023", "\u1024", "\u1025", "\u1026", "\u1025\u102E", "\u1027", "\u1029", "\u107E\u101E", "\u102A", "\u1031\u107E\u101E\u102C\u1039"};
+		String[] collationForms = new String[] {"\u1021\u102D", "\u1021\u102E", "\u1021\u102F", "\u1021\u1030", "\u1021\u1030", "\u1021\u1031", "\u1031\u1021\u102C", "\u1031\u1021\u102C", "\u1031\u1021\u102C\u1039", "\u1031\u1021\u102C\u1039"};
+
+		for (int i=0; i<independentVowels.length(); i++) {
+			if (this.rawText.contains(independentVowels[i])) {
+				segmentWord(this.rawText.replaceAll(independentVowels[i], collationForms[i]);
+				break;
+			} else if (i==independentVowels.length()-1) {
+				if (this.rawText.equals("\u1031\u101A\u102C\u1000\u1039\u103A\u102C\u1038"))
+					segmentWord("\u101A\u102C\u1000\u1039\u1000\u103A\u102C\u1038");
+				else if (this.rawText.equals("\u1000\u103C\u103A\u108F\u102F\u1039\u1015\u1039"))
+					segmentWord("\u1000\u103C\u103A\u108F\u102F\u1014\u102F\u1015\u1039");
+				else
+					segmentWord(this.rawText);
+			}
+		}
 	}
-	
-	
+
+
 	/**
 	 * Populates consonant/medial/vowell etc. fields
 	 * @param text
@@ -82,7 +78,7 @@ public class ZawgyiWord {
 				sortConsonant = sortConsonant.replaceAll("\u108F", "\u1014");
 			}
 		}
-		
+
 		//Segment medial
 		if (text.length()>1) {
 			Matcher m = medialRegex.matcher(text);
@@ -95,34 +91,34 @@ public class ZawgyiWord {
 				sortMedial = "-";
 			}
 		}
-		
+
 		//Segment vowell
-		
+
 		//Segment final
-		
+
 		//Segment tone
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 		//Finally
 		if (text.length()>1) {
 			sortUnknown = text;
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Returns the raw string, as typed by the user.
 	 */
 	public String toString() {
 		return rawText;
 	}
-	
+
 	/**
 	 * Returns the string used for sorting.
 	 */
@@ -132,8 +128,8 @@ public class ZawgyiWord {
 		else
 			return sortConsonant + sortMedial + sortFinal + sortVowel + sortTone + "(" + sortUnknown + ")";
 	}
-	
-	
+
+
 	//Useful static functions
 	public static void properlyOrder(String txt) {
 		//Re-order where necessary.
@@ -332,30 +328,30 @@ public class ZawgyiWord {
 			if (chIn>0 && chars[chIn]==0x108A && chars[chIn-1]==0x100A) {
 				chars[chIn-1]=0x106B;
 			}
-			
+
 			//Small fix 1072's a bit ugly at times
 			if (chIn>0 && chars[chIn-1]==0x1010 && chars[chIn]==0x1072) {
 				chars[chIn]=0x1071;
 			}
 		}
-		
+
 		txt = new String(chars);
 	}
-	
-	
+
+
 	public static boolean isInError(String txt) {
 		//Check for bad sequences, etc.
 		return false;
 	}
-	
+
 	/*public enum CHARACTER_WIDTH {
 		NOT_MYANMAR, UNKNOWN, ZERO_WIDTH, SOME_WIDTH
 	};
-	
+
 	public static CHARACTER_WIDTH getWidthClassifier(char mmChar) {
 		if (mmChar<'\u1000' || mmChar>'\u109F')
 			return CHARACTER_WIDTH.NOT_MYANMAR;
-		
+
 	}*/
 
 }
