361c7cd66891cd95fbebf447f4531e668538986e
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2008-06-10 13:50:33 +0000

    Finally runs...

--- eclipse_project/src/com/waitzar/analysis/Runner.java
+++ eclipse_project/src/com/waitzar/analysis/Runner.java
@@ -1,36 +1,116 @@
 package com.waitzar.analysis;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Runner {
 
 	public static void main(String[] args) {
-		HashMap<String, ArrayList<String>> rhymes = new HashMap<String, ArrayList<String>>();
+		HashMap<String, ArrayList<ZawgyiWord>> rhymes = new HashMap<String, ArrayList<ZawgyiWord>>();
 		
-		//NOTE: Take the two "contractions" out of the dictionary and make them their own special cases...
+		String fileName = "rhymes.txt";
+		if (args.length > 0) {
+			fileName = args[0];
+		}
 		
-		
-		
-		//Read all rhymes, and do:
-		//rhymes.get(word.toCanonString()).add(word);
-		//we should save the "examples", too...
-		
-		//Then, output:
-		//foreach canon : rhymes.keys
-		//  if (canon.containsChar('('))
-		//    continue;
-		//  foreach word : rhymes.get(canon) { print it, & its examples }
-		//  print a trailing newline
-		//print "Errors:"
-		//foreach (canon : rhymes.keys
-		//  if (!canon.containsChar('('))
-		//    continue;
-		//  print: canon
-		//  foreach word : rhymes.get(canon) { print it }
-		
+		//Open the file
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found.");
+			return; 
+		} catch (UnsupportedEncodingException ex1) {
+			System.out.println("Unsupported encoding (UTF-8).");
+			return;
+		}
 		
 
+		//Read each line
+		Pattern linePattern = Pattern.compile("([^ ]+)[^\t]+\t[^\t]+\t([^\t]+)");
+		try {
+			for (String line = reader.readLine(); line!=null; line=reader.readLine()) {
+				if (line.length()==0)
+					continue;
+				line = line.trim();
+				
+				//Parse
+				Matcher m = linePattern.matcher(line);
+				if (!m.matches()) {
+					throw new RuntimeException("Line doesn't match: " + ZawgyiWord.printMM(line));
+				}
+				//System.out.println("Raw: " + ZawgyiWord.printMM(m.group(1)));
+				ZawgyiWord rhyme = new ZawgyiWord(m.group(1));
+				rhyme.setData(m.group(2));
+	
+				//Add it
+				if (!rhymes.containsKey(rhyme.toCanonString())) {
+					rhymes.put(rhyme.toCanonString(), new ArrayList<ZawgyiWord>());
+				}
+				rhymes.get(rhyme.toCanonString()).add(rhyme);
+			}
+			reader.close();
+		} catch (IOException ex) {
+			System.out.println("IO exception");
+		}
+		
+		//Now, sort our key set. We have to meddle a bit with this one...
+		Set<String> keys = rhymes.keySet();
+		ArrayList<ZawgyiWord> firstWords = new ArrayList<ZawgyiWord>();
+		for (String key : keys) {
+			ZawgyiWord firstWord = rhymes.get(key).get(0);
+			firstWords.add(firstWord);
+		}
+		Collections.sort(firstWords, ZawgyiWord.getComparator());
+		
+		//Open output file
+		BufferedWriter writeFile = null;
+		try {
+			writeFile = new BufferedWriter(new PrintWriter(fileName + ".ordered", "UTF-8"));
+		} catch (FileNotFoundException ex) {
+			System.out.println("Out file not found.");
+		} catch (UnsupportedEncodingException ex) {
+			System.out.println("Out file encoding not supported (UTF-8).");
+		}
+		
+		//Print... saving unknowns
+		try {
+			ArrayList<ZawgyiWord> unknown = new ArrayList<ZawgyiWord>();
+			for (ZawgyiWord key : firstWords) {
+				writeFile.write(key.toCanonString()+"\n");
+				for (ZawgyiWord val : rhymes.get(key.toCanonString())) {
+					if (val.getUnknown().length()==0) {
+						writeFile.write(val.toString() + "\t" + val.getData() + "\n");
+					} else 
+						unknown.add(val);
+				}
+				writeFile.write("\n");
+			}
+			
+			//Print unknowns
+			writeFile.write("\nUNKNOWNS\n");
+			for (ZawgyiWord uk : unknown)
+				writeFile.write(uk.toString() + " (" + uk.getUnknown() + ")\n");
+			writeFile.close();
+		} catch (IOException ex) {
+			System.out.println("IO exception on output.");
+		}
+		
+		System.out.println("Done");
 	}
 
 }
