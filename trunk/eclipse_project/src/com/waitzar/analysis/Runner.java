4a013b5318475037682030a01f05330fdb1b218c
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2008-06-12 01:39:05 +0000

    Program sorts correctly, as far as I can tell.... now to print it! (Note: 100 rhymes are simple artifacts. Good news for memorization!)

--- trunk/eclipse_project/src/com/waitzar/analysis/Runner.java
+++ trunk/eclipse_project/src/com/waitzar/analysis/Runner.java
@@ -21,24 +21,24 @@ public class Runner {
 
 	public static void main(String[] args) {
 		HashMap<String, ArrayList<ZawgyiWord>> rhymes = new HashMap<String, ArrayList<ZawgyiWord>>();
-		
+
 		String fileName = "rhymes.txt";
 		if (args.length > 0) {
 			fileName = args[0];
 		}
-		
+
 		//Open the file
 		BufferedReader reader = null;
 		try {
 			reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
 		} catch (FileNotFoundException ex) {
 			System.out.println("File not found.");
-			return; 
+			return;
 		} catch (UnsupportedEncodingException ex1) {
 			System.out.println("Unsupported encoding (UTF-8).");
 			return;
 		}
-		
+
 
 		//Read each line
 		Pattern linePattern = Pattern.compile("([^ ]+)[^\t]+\t[^\t]+\t([^\t]+)");
@@ -47,7 +47,7 @@ public class Runner {
 				if (line.length()==0)
 					continue;
 				line = line.trim();
-				
+
 				//Parse
 				Matcher m = linePattern.matcher(line);
 				if (!m.matches()) {
@@ -56,7 +56,7 @@ public class Runner {
 				//System.out.println("Raw: " + ZawgyiWord.printMM(m.group(1)));
 				ZawgyiWord rhyme = new ZawgyiWord(m.group(1));
 				rhyme.setData(m.group(2));
-	
+
 				//Add it
 				if (!rhymes.containsKey(rhyme.toCanonString())) {
 					rhymes.put(rhyme.toCanonString(), new ArrayList<ZawgyiWord>());
@@ -67,7 +67,7 @@ public class Runner {
 		} catch (IOException ex) {
 			System.out.println("IO exception");
 		}
-		
+
 		//Now, sort our key set. We have to meddle a bit with this one...
 		Set<String> keys = rhymes.keySet();
 		ArrayList<ZawgyiWord> firstWords = new ArrayList<ZawgyiWord>();
@@ -76,9 +76,12 @@ public class Runner {
 			firstWords.add(firstWord);
 		}
 		Collections.sort(firstWords, ZawgyiWord.getComparator());
-		
+
 		//Open output file
 		BufferedWriter writeFile = null;
+		int totalRhymes = 0;
+		int uniqueRhymes = 0;
+
 		try {
 			writeFile = new BufferedWriter(new PrintWriter(fileName + ".ordered", "UTF-8"));
 		} catch (FileNotFoundException ex) {
@@ -86,30 +89,39 @@ public class Runner {
 		} catch (UnsupportedEncodingException ex) {
 			System.out.println("Out file encoding not supported (UTF-8).");
 		}
-		
+
 		//Print... saving unknowns
 		try {
 			ArrayList<ZawgyiWord> unknown = new ArrayList<ZawgyiWord>();
 			for (ZawgyiWord key : firstWords) {
+				uniqueRhymes++;
 				writeFile.write(key.toCanonString()+"\n");
 				for (ZawgyiWord val : rhymes.get(key.toCanonString())) {
+					totalRhymes++;
 					if (val.getUnknown().length()==0) {
 						writeFile.write(val.toString() + "\t" + val.getData() + "\n");
-					} else 
+					} else {
 						unknown.add(val);
+					}
 				}
 				writeFile.write("\n");
 			}
-			
+
 			//Print unknowns
 			writeFile.write("\nUNKNOWNS\n");
 			for (ZawgyiWord uk : unknown)
 				writeFile.write(uk.toString() + " (" + uk.getUnknown() + ")\n");
+
+			//Print stats
+			writeFile.write("\nSTATS\n");
+			writeFile.write("Unique Rhymes: " + uniqueRhymes + "\n");
+			writeFile.write("Total Rhymes: " + totalRhymes + "\n");
+
 			writeFile.close();
 		} catch (IOException ex) {
 			System.out.println("IO exception on output.");
 		}
-		
+
 		System.out.println("Done");
 	}
 
