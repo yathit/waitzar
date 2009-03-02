9ba97e087436cc85fa40fec91df73e9a8dbf9e58
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-03-02 14:55:13 +0000

    Updated the cross-platform library. We'll be using this new utilities file for various useful Unicode-related (and conversion-related) functions.

---
+++ cross_platform_library/waitzar/wz_utilities.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2009 by Seth N. Hetu
+ *
+ * Please refer to the end of the file for licensing information
+ */
+
+#ifndef _WZ_UTILITIES
+#define _WZ_UTILITIES
+
+//Don't let Visual Studio warn us to use the _s functions
+#define _CRT_SECURE_NO_WARNINGS
+
+//Necessary libraries
+#include <wchar.h>
+#include <stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////////
+// Please note that the wz_utilities library is currently considered
+//   to be UNSTABLE, and is not included in the Linux build by default.
+// This is because most of its functionality is of direct benefit to the
+//   Windows build, so we hope to test its correctness through the next
+//   two Windows releases of WaitZar. 
+// Since there is some universally-useful code here, we will eventually
+//   include this in both releases, but we want to avoid bumping libwaitzar's
+//   major revision for breakages caused by code that wasn't originally intended
+//   for Linux to begin with.
+//////////////////////////////////////////////////////////////////////////////////
+namespace waitzar 
+{
+
+
+
+
+} //End waitzar namespace
+
+
+#endif //_WZ_UTILITIES
+
+
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
