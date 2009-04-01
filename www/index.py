cead1da84c6c1b408e89b35a3d0bf5e104a032d2
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-04-01 13:39:43 +0000

    Sample web page.

---
+++ www/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import cgi
+
+def main():
+	print "Content-Type: text/html\n"
+	try:
+		import locus
+	except:
+		print "<html><head><title>Server-Side Error</title></head>\n<body>"
+		print "<!-- --><h1>Sorry, An Error Occurred</h1>\n"
+		print "Please try back in a few minutes; we are working hard to fix this problem.\n"
+		print "<br>&nbsp;\n"
+		print "<br>&nbsp;\n"
+		print "<br>&nbsp;\n"
+		print "<h1 style=\"color:#999999;\">Error Specifics</h1>\n<span style=\"color:#999999;\">"
+		
+		#Print our error in a safe way
+		cgi.print_exception()
+		
+		print "</span></body></html>"
+
+
+if __name__ == '__main__':
+	main()
