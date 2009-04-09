f49bac2cc36c78e5aa63ea4042cec972fedc47b7
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-04-09 12:21:43 +0000

    Ok, dynamic loading should work now.

--- www/download.py
+++ www/download.py
@@ -5,5 +5,17 @@ import cgi
 
 #Render our page
 execfile("index.py")
-safe_render("download.py")
+safe_render(__file__)
+
+
+#Doing it this way kind of gets around our security trick, unfortunately...
+#  However, it only omits syntax errors, which we should be checking on the command
+#  line anyway. It still catches runtime errors properly, to some degree.
+import locus
+class DownloadTemplate(locus.FallbackTemplate):
+	template = r'''
+		<h1>Download WaitZar</h1>
+		In progress...
+		'''
+
 
