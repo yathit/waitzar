783c7be067e799ebfea6e7a0724b183426e94dc6
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-04-13 09:14:07 +0000

    Debugging output...

--- www/locus.py
+++ www/locus.py
@@ -52,11 +52,14 @@ def render_a_page(pagename):
 			className = moduleName[0].capitalize() + moduleName[1:] + "Template"
 			
 			classInst = forname(moduleName + "_w", className)
-			
+
+			print "GOING"			
 			if fields.has_key("name") and fields.has_key("comments") and fields.has_key("email"):
+				print "GOING2"
 				bodyTxt = classInst(name=fields.getfirst('name'), email=fields.getfirst('email'), comments=fields.getfirst('comments'))
 			else:
 				bodyTxt = classInst()
+			print "GOING3"
 		else:
 			raise Exception
 	except (AttributeError, ImportError):
@@ -67,7 +70,7 @@ def render_a_page(pagename):
 		print bodyTxt
 	else:
 		#DEBUG
-		if True:
+		if False:
 			print "REQUEST_METHOD:", os.environ["REQUEST_METHOD"] , "<br>"
 			print "Values: <br>"
 			f = fields
