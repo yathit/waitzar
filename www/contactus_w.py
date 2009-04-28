5b0241ff8615fe716c42a7c4e531460e24992861
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-04-28 17:16:18 +0000

    Merged all stylesheets.

--- www/contactus_w.py
+++ www/contactus_w.py
@@ -8,35 +8,35 @@ class ContactusTemplate(FallbackTemplate):
 		<form name="wzRespForm">
 			<table border="0" rules="none" cellpadding="10" cellspacing="0" style="margin-top:20px; margin-left: 10px;">
 				<tr>
-					<td width="1" style="border-left:1px solid green; border-top:1px solid green;"><img src="img/wz_icon.png" width="48" height="48"/></td>
-					<td width="200" style="border-top:1px solid green; border-right:1px solid green;">
+					<td width="1" class="gll glt"><img src="img/wz_icon.png" width="48" height="48"/></td>
+					<td width="200" class="glt glr">
 						<span style="font-size: 12px;">Why are you contacting us?</span>
 						<br><select id="requestType" name="requestType" style="width:160;" onchange='javascript:setLabels();'>
 							<option value="comment" selected>Feedback</option>
 							<option value="help">Help Request</option>
 						</select>
 					</td>
-					<td style="border-bottom:1px solid green;">&nbsp;</td>
-					<td style="border-bottom:1px solid green;">&nbsp;</td>
+					<td class="glb">&nbsp;</td>
+					<td class="glb">&nbsp;</td>
 				</tr>
 				<tr>
-					<td width="260" style="border-left:1px solid green; border-bottom:1px solid green;" colspan="3" rowspan="3">
+					<td width="260" class="gll glb" colspan="3" rowspan="3">
 						<span id="commentsTag" style="font-size: 12px;">Please type your feedback here:</span>
 						<textarea id="comments" onmousedown="unRedElement('comments');" name="comments" cols="40" rows="10"></textarea>
 					</td>
-					<td style="border-right:1px solid green;" height="1">
+					<td class="glr" style="height="1">
 						<span id="nameTag" style="font-size: 12px;">Your Name:</span>
 						<br><input id="userName"  onmousedown="unRedElement('userName');"  name="userName" type="text" style="width: 200px;"/>
 					</td>
 				</tr>
 				<tr>
-					<td style="border-right:1px solid green;" height="1">
+					<td class="glr" style="height="1">
 						<span id="emailTag" style="font-size: 12px;">Your Email Address: (optional)</span>
 						<br><input id="userEmail"  onmousedown="unRedElement('userEmail');"  name="userEmail" type="text" style="width: 200px;"/>
 					</td>
 				</tr>
 				<tr>
-					<td style="border-right:1px solid green; border-bottom:1px solid green;" valign="middle" align="center">
+					<td class="glr glb" valign="middle" align="center">
 						<input style="font-size: 18px;" id="submitButton" value="Submit Feedback" type="button" onclick='JavaScript:validateRequest();'/>
 						<div id="submitLoader"></div>
 					</td>
--- www/getinvolved_w.py
+++ www/getinvolved_w.py
@@ -10,43 +10,43 @@ class GetinvolvedTemplate(FallbackTemplate):
 
         <h1>How Can I Help?</h1>
 
-		<table border="0" cellpadding="00" cellspacing="0" style="margin-bottom:20px; margin-top:10px;">
+		<table border="0" cellpadding="00" cellspacing="0" class="helptbl">
 			<tr>
-				<td width="1" valign="top" align="left"  style="padding-left:10px; padding-top:10px;">
-					<a style="text-decoration: none;" href="http://code.google.com/p/waitzar/issues/list">
+				<td width="1" valign="top" align="left"  class="pl10 pt10">
+					<a class="ninjalink" href="http://code.google.com/p/waitzar/issues/list">
 						<img src="img/wz_bug.png" border="0"/>
 					</a>
 				</td>
-				<td width="100%" valign="top" align="left"  style="padding-top:10px; padding-left: 10px;">
-					<a style="" href="http://code.google.com/p/waitzar/issues/list">
-						<span style="font-size: 1.25em; color: #00CCFF;">Report a Bug / Ask for a Feature</span>
+				<td width="100%" valign="top" align="left"  class="pt10 pl10">
+					<a class="ninjalink" href="http://code.google.com/p/waitzar/issues/list">
+						<span class="helpbigtxt">Report a Bug / Ask for a Feature</span>
 					</a>
 				</td>
 			</tr>
 			<tr>
-				<td colspan="2" style="color:white; padding-left: 10px; padding-bottom:10px; padding-top: 8px;">
+				<td colspan="2" class="helptinytxt">
 					WaitZar is well-tested, but that doesn't mean there are no bugs. If you find a bug white using WaitZar, please click the link above and post a "New Issue" describing your problem in great detail. Similarly, you might be using WaitZar and say "Gee, I wish I could (do something)" &mdash;if so, please post an Issue describing your feature request. Most of WaitZar's best features were suggested by the community, including the "Help" keyboard, cursor tracking, and even the custom dictionary "mywords.txt". We'd love to hear your suggestions.
 				</td>
 			</tr>
 		</table>
 
-		<table border="0" cellpadding="00" cellspacing="0" style="margin-bottom:20px; margin-top:10px;">
+		<table border="0" cellpadding="00" cellspacing="0" class="helptbl">
 			<tr>
-				<td width="1" valign="top" align="left"  style="padding-left:10px; padding-top:10px;">
-					<a style="text-decoration: none;" href="http://waitzar.googlecode.com/files/wz_usericon.png">
+				<td width="1" valign="top" align="left"  class="pl10 pt10">
+					<a class="ninjalink" href="http://waitzar.googlecode.com/files/wz_usericon.png">
 						<img src="http://waitzar.googlecode.com/files/wz_usericon.png" border="0"/>
 					</a>
 				</td>
-				<td width="100%" valign="top" align="left"  style="padding-top:10px; padding-left: 10px;">
-					<a style="" href="http://waitzar.googlecode.com/files/wz_usericon.png">
-						<span style="font-size: 1.25em; color: #00CCFF;">Use the WaitZar Blog Sticker</span>
+				<td width="100%" valign="top" align="left"  class="pl10 pt10">
+					<a class="ninjalink" href="http://waitzar.googlecode.com/files/wz_usericon.png">
+						<span class="helpbigtxt">Use the WaitZar Blog Sticker</span>
 					</a>
 				</td>
 			</tr>
 			<tr>
-				<td colspan="2" style="color:white; padding-left: 10px; padding-bottom:10px; padding-top: 8px;">
+				<td colspan="2" class="helptinytxt">
 					If you find WaitZar useful, maybe other people would too? Put the WaitZar blog sticker in your forum signature, on your blog, or anywhere else that's relevant, and people who browse to your page will see it and try WaitZar themselves. You can use the following HTML code to link in the sticker:
-					<code style="color:#88FF99; font-size:1.2em;">
+					<code class="helpbigtxt">
 					<br>&nbsp;<br>&lt;a href="http://www.waitzar.com/"&gt;
 					<br>&nbsp;&nbsp;&nbsp;&lt;img border="0" src="http://waitzar.googlecode.com/files/wz_usericon.png"/&gt;
 					<br>&lt;/a&gt;
@@ -55,42 +55,42 @@ class GetinvolvedTemplate(FallbackTemplate):
 			</tr>
 		</table>
 
-		<table border="0" cellpadding="00" cellspacing="0" style="margin-bottom:20px; margin-top:10px;">
+		<table border="0" cellpadding="00" cellspacing="0" class="helptbl">
 			<tr>
-				<td width="1" valign="top" align="left"  style="padding-left:10px; padding-top:10px;">
-					<a style="text-decoration: none;" href="http://code.google.com/p/waitzar/source/checkout">
+				<td width="1" valign="top" align="left"  class="pl10 pt10">
+					<a class="ninjalink" href="http://code.google.com/p/waitzar/source/checkout">
 						<img src="img/wz_source.png" border="0"/>
 					</a>
 				</td>
-				<td width="100%" valign="top" align="left"  style="padding-top:10px; padding-left: 10px;">
-					<a style="" href="http://code.google.com/p/waitzar/source/checkout">
-						<span style="font-size: 1.25em; color: #00CCFF;">Play With the Source</span>
+				<td width="100%" valign="top" align="left"  class="pl10 pt10">
+					<a class="ninjalink" href="http://code.google.com/p/waitzar/source/checkout">
+						<span class="helpbigtxt">Play With the Source</span>
 					</a>
 				</td>
 			</tr>
 			<tr>
-				<td colspan="2" style="color:white; padding-left: 10px; padding-bottom:10px; padding-top: 8px;">
+				<td colspan="2" class="helptinytxt">
 					WaitZar is not only free, it is also "Open Source", which means that you can re-build the entire project yourself if you want to. Some of our users are quite talented programmers, and have, in the past, submitted patches to fix minor problems with the source code. If you want to contribute, that would be great! We know how much time goes into writing good code, so we treat all patch requests seriously.
 				</td>
 			</tr>
 		</table>
 
 
-		<table border="0" cellpadding="00" cellspacing="0" style="margin-bottom:20px; margin-top:10px;">
+		<table border="0" cellpadding="00" cellspacing="0" class="helptbl">
 			<tr>
-				<td width="1" valign="top" align="left"  style="padding-left:10px; padding-top:10px;">
+				<td width="1" valign="top" align="left"  class="pl10 pt10">
 
 						<img src="img/wz_friends.png" border="0"/>
 
 				</td>
-				<td width="100%" valign="top" align="left"  style="padding-top:10px; padding-left: 10px;">
+				<td width="100%" valign="top" align="left"  class="pl10 pt10">
 
-						<span style="font-size: 1.25em; color: #00CCFF;">Tell Your Friends</span>
+						<span class="helpbigtxt">Tell Your Friends</span>
 
 				</td>
 			</tr>
 			<tr>
-				<td colspan="2" style="color:white; padding-left: 10px; padding-bottom:10px; padding-top: 8px;">
+				<td colspan="2" class="helptinytxt">
 					Right now, the primary users of WaitZar are Burmese with an interest in computers and technology. In fact, we designed WaitZar to be easy to use for anyone who knows how to send email. So, we'd like you to share your experiences with others. If you have friends who can't be bothered to learn a Burmese keyboard layout, encourage them to try WaitZar. It's easy to use and quick to learn, so it's a great way for casual typists to start sending emails in Burmese.
 				</td>
 			</tr>
