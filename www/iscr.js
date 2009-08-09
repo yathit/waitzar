3efb2fa0d8ac8c6dd6db30b429835785b2450ad5
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-08-09 19:02:01 +0000

    JSLint'd most of the errors out (a few implied globals).

--- www/iscr.js
+++ www/iscr.js
@@ -14,13 +14,18 @@
         img4.src="img/loading.gif";
       }
 
-
       //An array of our buttons
-      var buttons=new Array (
+      var buttons = [
         "img/spotlight.png",
         "img/spotlight_over.png",
         "img/spotlight_down.png"
-      );
+      ];
+      
+      //Some globals
+	var errorCount = 0;
+	var errorAddCount = 0;
+	var errorRemoveCount = 0;
+	var qstr;
 
 
       //Swap image "name" with button id "item"
@@ -61,64 +66,7 @@
         document.getElementById(name+"Txt").style.textShadow = "#000088 -1.5px -1.5px 1.5px";
         return true;
       }
-
-
-      //Simple AJAX functionality
-      function xmlhttpPost(strURL, formName) {
-        if (formName == "wzRespForm") {
-      		document.getElementById("submitButton").disabled = true;
-	      	document.getElementById("submitButton").value = "Please Wait"
-      		document.getElementById("submitLoader").innerHTML = "<img src=\"img/loading.gif\"/>";
-      	} else if (formName == "wzNewSiteForm") {
-      		document.getElementById("submitAddButton").disabled = true;
-	      	document.getElementById("submitAddButton").value = "Please Wait"
-      		document.getElementById("submitAddLoader").innerHTML = "<img src=\"img/loading.gif\"/>";
-      	}
-
-        var xmlHttpReq = false;
-        var self = this;
-        var params = getquerystring(formName);
-        if (window.XMLHttpRequest) {
-          //Mozilla/Safari
-          self.xmlHttpReq = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-          //IE
-          self.xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        self.xmlHttpReq.open('POST', strURL, true);
-        self.xmlHttpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		//self.xmlHttpReq.setRequestHeader("Content-length", params.length);
-		//self.xmlHttpReq.setRequestHeader("Connection", "close");
-
-        self.xmlHttpReq.onreadystatechange = function() {
-          if (self.xmlHttpReq.readyState == 4) {
-            updatepage(self.xmlHttpReq.responseText);
-          }
-        }
-        self.xmlHttpReq.send(params);
-      }
-
-      function xmlhttpGet(strURL) {
-        document.getElementById("main").innerHTML = "<img src=\"img/loading.gif\"/>";
-
-        var xmlHttpReq = false;
-        var self = this;
-        if (window.XMLHttpRequest) {
-          //Mozilla/Safari
-          self.xmlHttpReq = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-          //IE
-          self.xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        self.xmlHttpReq.open('GET', strURL+"?partial=true", true);
-        self.xmlHttpReq.onreadystatechange = function() {
-          if (self.xmlHttpReq.readyState == 4) {
-            updatepage(self.xmlHttpReq.responseText);
-          }
-        }
-        self.xmlHttpReq.send("");
-      }
-
+      
       function getquerystring(formName) {
         var form     = document.forms[formName];
         
@@ -137,36 +85,96 @@
 		   "link" : form.webSiteURL.value
 		};
 		if (form.webSiteImage.value.length>0) {
-			jsonObj['img'] = form.webSiteImage.value;
+			jsonObj.img = form.webSiteImage.value;
 		}
 
 	
-		name = "WZ Site Add Request"
+		name = "WZ Site Add Request";
 		email = "";
-		comments = "Site Name: " + form.webSiteName.value + "\n" 
-			 + "Site URL: " + form.webSiteURL.value + "\n" 
-			 + "Site Image: " + form.webSiteImage.value + "\n\n"
-			 + "JSON Line: " + jsonObj.toJSONString() + "\n";
+		comments = "Site Name: " + form.webSiteName.value + "\n" +
+			 "Site URL: " + form.webSiteURL.value + "\n" +
+			 "Site Image: " + form.webSiteImage.value + "\n\n" +
+			 "JSON Line: " + jsonObj.toJSONString() + "\n";
       	}
         
-        if (email.length==0) {
-          email = "help@waitzar.com"
+        if (email.length===0) {
+          email = "help@waitzar.com";
         }
 
         // NOTE: no '?' before querystring
         qstr = 'name=' + escape(name) + '&email=' + escape(email) + '&comments=' + escape(comments) + '&partial=yes';
         return qstr;
       }
-
+      
       function updatepage(str){
         document.getElementById("main").innerHTML = str;
       }
 
 
+
+      //Simple AJAX functionality
+      function xmlhttpPost(strURL, formName) {
+        if (formName == "wzRespForm") {
+      		document.getElementById("submitButton").disabled = true;
+	      	document.getElementById("submitButton").value = "Please Wait";
+      		document.getElementById("submitLoader").innerHTML = "<img src=\"img/loading.gif\"/>";
+      	} else if (formName == "wzNewSiteForm") {
+      		document.getElementById("submitAddButton").disabled = true;
+	      	document.getElementById("submitAddButton").value = "Please Wait";
+      		document.getElementById("submitAddLoader").innerHTML = "<img src=\"img/loading.gif\"/>";
+      	}
+
+        //var xmlHttpReq = false; //Unused?
+        var self = this;
+        var params = getquerystring(formName);
+        if (window.XMLHttpRequest) {
+          //Mozilla/Safari
+          self.xmlHttpReq = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+          //IE
+          self.xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        self.xmlHttpReq.open('POST', strURL, true);
+        self.xmlHttpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		//self.xmlHttpReq.setRequestHeader("Content-length", params.length);
+		//self.xmlHttpReq.setRequestHeader("Connection", "close");
+
+        self.xmlHttpReq.onreadystatechange = function() {
+          if (self.xmlHttpReq.readyState == 4) {
+            updatepage(self.xmlHttpReq.responseText);
+          }
+        };
+        self.xmlHttpReq.send(params);
+      }
+
+      function xmlhttpGet(strURL) {
+        document.getElementById("main").innerHTML = "<img src=\"img/loading.gif\"/>";
+
+        //var xmlHttpReq = false;//Unused?
+        var self = this;
+        if (window.XMLHttpRequest) {
+          //Mozilla/Safari
+          self.xmlHttpReq = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+          //IE
+          self.xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        self.xmlHttpReq.open('GET', strURL+"?partial=true", true);
+        self.xmlHttpReq.onreadystatechange = function() {
+          if (self.xmlHttpReq.readyState == 4) {
+            updatepage(self.xmlHttpReq.responseText);
+          }
+        };
+        self.xmlHttpReq.send("");
+      }
+
+
+
+
 			//This script is used by contactus.py, since AJAX-ing it doesn't seem to work.
 		  //Set the text of our labels based on the context
 		  function setLabels(){
-		    if (document.getElementById("requestType").selectedIndex == 0) {
+		    if (document.getElementById("requestType").selectedIndex === 0) {
 				document.getElementById("commentsTag").innerHTML = "Please type your feedback here:";
 				document.getElementById("emailTag").innerHTML = "Your Email Address: (optional)";
 				document.getElementById("submitButton").value = "Submit Feedback";
@@ -177,29 +185,26 @@
 		    }
 		  }
 
-		  var errorCount = 0;
-		  var errorAddCount = 0;
-		  var errorRemoveCount = 0;
 		  
 		  function validateAddRequest() {
 		    //Reset all borders
 		  	errorAddCount = 0;
 
-		  	if (document.getElementById("webSiteName").value.length==0) {
+		  	if (document.getElementById("webSiteName").value.length===0) {
   		  		document.getElementById("webSiteName").style.border = "3px solid red";
   		  		document.getElementById("webSiteName").style.backgroundColor = "#FFCCCC";
   		  		document.getElementById("webSiteName").value = "Please enter a name for this web site.";
   		  		errorAddCount = errorAddCount + 1;
 		  	}
 		  	
-		  	if (document.getElementById("webSiteURL").value.length==0) {
+		  	if (document.getElementById("webSiteURL").value.length===0) {
   		  		document.getElementById("webSiteURL").style.border = "3px solid red";
   		  		document.getElementById("webSiteURL").style.backgroundColor = "#FFCCCC";
   		  		document.getElementById("webSiteURL").value = "Please enter this web site's URL.";
   		  		errorAddCount = errorAddCount + 1;
 		  	}
 		  	
-		    	if (errorAddCount==0) {
+		    	if (errorAddCount===0) {
 				xmlhttpPost("sitelinks.py", "wzNewSiteForm");
 			} else {
 				document.getElementById("submitAddButton").disabled = true;
@@ -214,7 +219,7 @@
 				errorAddCount = errorAddCount - 1;
 			}
 
-		    if (errorAddCount==0) {
+		    if (errorAddCount===0) {
 				document.getElementById("submitAddButton").disabled = false;
 			}
 		  }
@@ -229,21 +234,21 @@
 		    //Reset all borders
 		  	errorCount = 0;
 
-		  	if (document.getElementById("comments").value.length==0) {
+		  	if (document.getElementById("comments").value.length===0) {
   		  		document.getElementById("comments").style.border = "3px solid red";
   		  		document.getElementById("comments").style.backgroundColor = "#FFCCCC";
   		  		document.getElementById("comments").value = "Please enter a brief description.";
   		  		errorCount = errorCount + 1;
 		  	}
 
-		  	if (document.getElementById("userName").value.length==0) {
+		  	if (document.getElementById("userName").value.length===0) {
   		  		document.getElementById("userName").style.border = "3px solid red";
   		  		document.getElementById("userName").style.backgroundColor = "#FFCCCC";
   		  		document.getElementById("userName").value = "Please type your name.";
   		  		errorCount = errorCount + 1;
 		  	}
 
-		  	if (document.getElementById("userEmail").value.length==0 && document.getElementById("requestType").selectedIndex == 1) {
+		  	if (document.getElementById("userEmail").value.length===0 && document.getElementById("requestType").selectedIndex == 1) {
   		  		document.getElementById("userEmail").style.border = "3px solid red";
   		  		document.getElementById("userEmail").style.backgroundColor = "#FFCCCC";
   		  		document.getElementById("userEmail").value = "Please type your email.";
@@ -252,7 +257,7 @@
 
 
 
-		    if (errorCount==0) {
+		    if (errorCount===0) {
 				xmlhttpPost("contactus.py", "wzRespForm");
 			} else {
 				document.getElementById("submitButton").disabled = true;
@@ -268,7 +273,7 @@
 				errorCount = errorCount - 1;
 			}
 
-		    if (errorCount==0) {
+		    if (errorCount===0) {
 				document.getElementById("submitButton").disabled = false;
 			}
 		  }
