5a394c6dac4fc4c13019b3b47a12c1a3a50ec51f
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-11-18 06:49:21 +0000

    Our converter now loads multiple definitions of getWordKeyStrokes. It also is wrapped in the waitzar namespace to avoid pollution (maybe wrap it in a hidden namespace?)

--- cross_platform_library/waitzar/PythonSpecs.cpp
+++ cross_platform_library/waitzar/PythonSpecs.cpp
@@ -15,14 +15,33 @@
 using namespace boost::python;
 
 
+namespace waitzar
+{
+
+
 //Built-in integration with Boost.Python
 // For now, call it "libwaitzar" so we don't have to rename the shared library
 BOOST_PYTHON_MODULE(libwaitzar)
 {
+    //Overloaded functions
+    std::wstring    (waitzar::WordBuilder::*wk1)(unsigned int)                            = &waitzar::WordBuilder::getWordKeyStrokes;
+    std::wstring    (waitzar::WordBuilder::*wk2)(unsigned int, unsigned int)    = &waitzar::WordBuilder::getWordKeyStrokes;
+
     class_<waitzar::WordBuilder>("WordBuilder", init< const char*, std::vector<std::string> >())
         .def("typeLetter", &waitzar::WordBuilder::typeLetter)
         .def("reset", &waitzar::WordBuilder::reset)
+	.def("getParenString", &waitzar::WordBuilder::getParenString)
+	.def("getPossibleWords", &waitzar::WordBuilder::getPossibleWords)
+	.def("getWordKeyStrokes", wk1)
+	.def("getWordKeyStrokes", wk2)
+	.def("getCurrSelectedID", &waitzar::WordBuilder::getCurrSelectedID)
     ;
+
+    /*enum_<waitzar::ENCODING>("encoding")
+        .value("unicode", waitzar::ENCODING.ENCODING_UNICODE)
+	.value("zawgyi", waitzar::ENCODING.ENCODING_ZAWGYI)
+	.value("wininnwa", waitzar::ENCODING.ENCODING_WININNWA)
+    ;*/
 	
     //Vector of strings
     class_<std::vector<std::string> >("StringVec")
@@ -30,3 +49,5 @@ BOOST_PYTHON_MODULE(libwaitzar)
     ;
 }
 
+
+}
