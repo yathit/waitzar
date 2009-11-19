76489bfc659184a06d79bcd4ac99bb566495a75e
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-11-19 12:07:05 +0000

    More definitions: properties, etc.

--- trunk/cross_platform_library/waitzar/PythonSpecs.cpp
+++ trunk/cross_platform_library/waitzar/PythonSpecs.cpp
@@ -24,9 +24,13 @@ namespace waitzar
 // For now, call it "libwaitzar" so we don't have to rename the shared library
 BOOST_PYTHON_MODULE(libwaitzar)
 {
-    //Overloaded functions
+    //Overloaded functions: getWordKeyStrokes
     std::wstring    (waitzar::WordBuilder::*wk1)(unsigned int)                            = &waitzar::WordBuilder::getWordKeyStrokes;
     std::wstring    (waitzar::WordBuilder::*wk2)(unsigned int, unsigned int)    = &waitzar::WordBuilder::getWordKeyStrokes;
+
+    //Overloaded functions: moveCursorRight
+    bool    (waitzar::SentenceList::*cr1)(int, bool, WordBuilder&)   = &waitzar::SentenceList::moveCursorRight;
+    bool    (waitzar::SentenceList::*cr2)(int, WordBuilder&)             = &waitzar::SentenceList::moveCursorRight;
 
     class_<waitzar::WordBuilder>("WordBuilder", init< const char*, std::vector<std::string> >())
         .def("typeLetter", &waitzar::WordBuilder::typeLetter)
@@ -36,6 +40,24 @@ BOOST_PYTHON_MODULE(libwaitzar)
 	.def("getWordKeyStrokes", wk1)
 	.def("getWordKeyStrokes", wk2)
 	.def("getCurrSelectedID", &waitzar::WordBuilder::getCurrSelectedID)
+	.def("typeSpace", &waitzar::WordBuilder::typeSpace)
+    ;
+	
+    class_<waitzar::SentenceList>("SentenceList")
+        .def("clear", &waitzar::SentenceList::clear)
+        .def("insert", &waitzar::SentenceList::insert)
+	.def("getCursorIndex", &waitzar::SentenceList::getCursorIndex)
+	.def("moveCursorRight",cr1)
+	.def("moveCursorRight", cr2)
+	.def("size", &waitzar::SentenceList::size)
+	.def("deleteNext", &waitzar::SentenceList::deleteNext)
+	.def("deletePrev", &waitzar::SentenceList::deletePrev)
+
+        //Iterators play differently in C++ and Python
+        .property("words", range(&waitzar::SentenceList::begin, &waitzar::SentenceList::end))
+
+        //Sometimes needed for consistency
+	.def("updateTrigrams", &waitzar::SentenceList::updateTrigrams)
     ;
 
     enum_<waitzar::ENCODING>("encoding")
@@ -48,6 +70,18 @@ BOOST_PYTHON_MODULE(libwaitzar)
     class_<std::vector<std::string> >("StringVec")
         .def(vector_indexing_suite<std::vector<std::string> >())
     ;
+
+    //Vector of unsigned integers
+    class_<std::vector<unsigned int> >("UIntVec")
+        .def(vector_indexing_suite<std::vector<unsigned int> >())
+    ;
+
+    //Pair of bool, unsigned int
+    typedef std::pair<bool, unsigned int> BoolUintPair;
+    to_python_converter< BoolUintPair, PairToTupleConverter<bool, unsigned int> >();
+    //class_<std::pair<bool, unsigned int> >("BooUIntPt")
+    //    .def(vector_indexing_suite<std::pair<bool, unsigned int> >())
+    //;
 }
 
 
