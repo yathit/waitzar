b072e93c871c499c7d8f90de3b6b39b2d123cddb
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-11-26 13:36:44 +0000

    Added function stubs and comments relating to suggested directory structure.
    Next step: re-write WaitZar's current configuration (including encodings, etc.) to use this directory structure, and test that WaitZar loads this properly.

--- Config WZ/ConfigManager.cpp
+++ Config WZ/ConfigManager.cpp
@@ -7,13 +7,92 @@
 
 #include "ConfigManager.h"
 
-ConfigManager::ConfigManager(void)
+using std::vector;
+using std::pair;
+using std::wstring;
+
+
+ConfigManager::ConfigManager(void){}
+
+ConfigManager::~ConfigManager(void){}
+
+
+/**
+ * Load our maing config file:
+ *   config/config.txt
+ *
+ * This file usually only contains SETTINGS (not languages, etc.) but for the
+ *   purose of brevity, we can actually load the entire configuration (inc.
+ *   languages, keyboards, etc.) here.
+ * This is the only config. file that is actually required.
+ */
+void ConfigManager::initMainConfig(const std::string& configFile)
 {
 }
 
-ConfigManager::~ConfigManager(void)
+
+/**
+ * Load all config files for a language:
+ *   config/Myanmar/
+ *                 config.txt
+ *                 ZgInput/config.txt
+ *                 StdTransformers/config.txt
+ *                 Burglish/config.txt
+ *   ...etc.
+ *
+ * These files may not contain general SETTINGS. The first config file (directly in the Language
+ *   folder) must contain basic language information (like the id). All other config files must be in
+ *   immediate sub-directories; beyond that, the structure is arbitrary. For example, we load "Burglish"
+ *   and "Zawgyi Input" in separate folders, but we load a series of "Standard Transformers" all from
+ *   one directory.
+ * These files are optional, but heavily encouraged in all distributions except the Web Demo.
+ */
+void ConfigManager::initAddLanguage(const std::string& configFile, const std::vector<std::string>& subConfigFiles)
 {
 }
+
+
+/**
+ * Load the application-maintained settings override file:
+ *   %USERPROFILE%\AppData\Local\WaitZar\config.override.txt
+ *   (actually, calls SHGetKnownFolderPath(FOLDERID_LocalAppData) \ WaitZar\config.override.txt)
+ *
+ * This json config file contains one single array of name-value pairs. The names are fully-qualified:
+ *   "language.myanmar.defaultdisplayencoding" = "zawgyi-one", for example. These override all 
+ *   WaitZar config options where applicable.
+ * WaitZar's GUI config window will alter this file. Loading it is optional, but it's generally a good idea
+ *  (otherwise, users' settings won't get loaded when they exit and re-load WaitZar).
+ */
+void ConfigManager::initLocalConfig(const std::string& configFile)
+{
+}
+
+
+/**
+ * Load the user-maintained settings override file:
+ *   %USERPROFILE%\Documents\waitzar.config.txt
+ *   (actually, calls SHGetKnownFolderPath(FOLDERID_Documents) \ waitzar.config.txt)
+ *
+ * This json config file contains one single array of name-value pairs. The names are fully-qualified:
+ *   "settings.showballoon" = "false", for example. These override all of WaitZar's config options, 
+ *   AND they override the settings in LocalConfig (see initAddLocalConfig). They can contain any
+ *   options, but are really only intended only to contain settings overrides (not language overrides, etc.)
+ * This is the file that users will tweak on their own. It is HIGHLY recommended to load this file, if it exists.
+ */
+void ConfigManager::initUserConfig(const std::string& configFile)
+{
+}
+
+
+
+
+//Not yet defined:
+vector< pair<wstring, wstring> > ConfigManager::getSettings() const {}
+vector<wstring> ConfigManager::getLanguages() const {}
+vector<wstring> ConfigManager::getInputManagers() const {}
+vector<wstring> ConfigManager::getEncodings() const {}
+wstring ConfigManager::getActiveLanguage() const {}
+void ConfigManager::changeActiveLanguage(const wstring& newLanguage) {}
 
 
 
