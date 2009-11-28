a698f363a11b3689a7417e5d0ba455e8452c6591
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-11-28 11:35:44 +0000

    Added stubs for actual methods in WaitZar. This proves isolation of our libraries. It also compiles nice and tiny (0.01 mb more than before).
    Next, we have to load real config options. And then actually integrate with WaitZar.... and then convert WZ's dictionary to Unicode.... so much left before we hav any screenshots. :(

---
+++ trunk/win32_source/Input/WaitZar.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2009 by Seth N. Hetu
+ *
+ * Please refer to the end of the file for licensing information
+ */
+
+#ifndef _INPUT_WAITZAR
+#define _INPUT_WAITZAR
+
+#include "Interfaces.h"
+
+/**
+ * Placeholder class: to be used for WaitZar Input method
+ */
+class WaitZar : public InputMethod
+{
+public:
+	WaitZar();
+
+	//Temp
+	void getString();
+};
+
+
+#endif //_INPUT_WAITZAR
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
