017ae659454188ebe441dee776908bf7ae0a5746
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2009-12-01 14:20:30 +0000

    Moved most settings around in VS's solution explorer. Unfortunately, we cannot maintain the VS2003/VS2005 solutions any more, since I no longer have a copy (and my license will expire soon anyway).
    VS2008 is the only supported build platform for now, although I will definitely consider writing a scons build file for those who hate VS. ;)

---
+++ win32_source/Settings/ConfigManager.h
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2009 by Seth N. Hetu
+ *
+ * Please refer to the end of the file for licensing information
+ */
+
+#ifndef _CONFIGMANAGER
+#define _CONFIGMANAGER
+
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <functional>
+#include <locale>
+#include "wz_utilities.h"
+#include "json_spirit_value.h"
+#include "Interfaces.h"
+
+
+//json_spirit_reader is defined elsewhere
+namespace json_spirit {
+	extern bool read( const std::wstring& s, wValue& value );
+}
+
+
+//Simple class to help us load json files easier
+class JsonFile {
+public:
+	JsonFile(const std::string& path="")
+	{
+		this->path = path;
+	}
+	json_spirit::wValue json() const
+	{
+		if (!hasParsed) {
+			const std::wstring text = waitzar::readUTF8File(path);
+			json_spirit::read(text, root);
+			hasParsed = true;
+		}
+		return root;
+	}
+	std::string getPath() const
+	{
+		return this->path;
+	}
+	//For map indexing:
+	bool operator<(const JsonFile& j) const
+	{
+		return this->path < j.path;
+	}
+private:
+	std::string path;
+	mutable json_spirit::wValue root;
+	mutable bool hasParsed;
+};
+
+
+
+
+
+//Options for our ConfigManager class
+struct Settings {
+	Option<std::wstring> hotkey;
+	Option<bool> silenceMywordsErrors;
+	Option<bool> balloonStart;
+	Option<bool> alwaysElevate;
+	Option<bool> trackCaret;
+	Option<bool> lockWindows;
+};
+struct Language {
+	Option<std::wstring> displayName;
+	Option<std::wstring> defaultOutputEncoding;
+	Option<std::wstring> defaultDisplayMethod;
+	Option<std::wstring> defaultInputMethod;
+	std::map<std::wstring, InputMethod*> inputMethods;
+	std::map<std::wstring, Encoding> encodings;
+	std::map<std::wstring, Transformation*> transformations;
+	std::map<std::wstring, DisplayMethod*> displayMethods;
+};
+struct OptionTree {
+	Settings settings;
+	std::map<std::wstring, Language> languages;
+};
+
+
+//Utility enum
+enum WRITE_OPTS {WRITE_MAIN, WRITE_LOCAL, WRITE_USER};
+
+
+/**
+ * This class exists for managing configuration options automatically. It is also
+ *  the main access point for WaitZar into the SpiritJSON library, without requiring 
+ *  us to link against Boost.
+ */
+class ConfigManager
+{
+public:
+	ConfigManager(void);
+	~ConfigManager(void);
+
+	//Build our config. manager up slowly
+	void initMainConfig(const std::string& configFile);
+	void initAddLanguage(const std::string& configFile, const std::vector<std::string>& subConfigFiles);
+	void initLocalConfig(const std::string& configFile);
+	void initUserConfig(const std::string& configFile);
+
+	//Accessible by our outside class
+	Settings getSettings();
+	std::vector<std::wstring> getLanguages() const;
+	std::vector<std::wstring> getInputManagers() const;
+	std::vector<std::wstring> getEncodings() const;
+
+	//Control
+	std::wstring getActiveLanguage() const;
+	void changeActiveLanguage(const std::wstring& newLanguage);
+	void loc_to_lower(std::wstring& str);
+
+
+private:
+	void readInConfig(json_spirit::wValue root, std::wstring context, WRITE_OPTS writeTo);
+	std::wstring sanitize_id(const std::wstring& str);
+	std::wstring sanitize(const std::wstring& str);
+	std::string escape_wstr(const std::wstring& str) const;
+	bool read_bool(const std::wstring& str);
+	void setSingleOption(const std::wstring& name, const std::wstring& value);
+
+private:
+	//Our many config files.
+	JsonFile mainConfig;
+	std::map<JsonFile , std::vector<JsonFile> > langConfigs;
+	JsonFile localConfig;
+	JsonFile userConfig;
+
+	//The actual representation
+	OptionTree options;
+};
+
+
+
+//Helper predicate
+template <class T> 
+class is_id_delim : public std::unary_function<T, bool>
+{
+public:
+ bool operator ()(T t) const
+ {
+  if ((t==' ')||(t=='\t')||(t=='\n')||(t=='-')||(t=='_'))
+   return true; //Remove it
+  return false; //Don't remove it
+ }
+};
+
+
+//And finally, locale-driven nonsense with to_lower:
+template<class T>
+class ToLower {
+public:
+     ToLower(const std::locale& loc):loc(loc)
+     {
+     }
+     T operator()(const T& src) const
+     {
+          return std::tolower<T>(src, loc);
+     }
+protected:
+     const std::locale& loc;
+};
+
+
+
+
+
+#endif //_CONFIGMANAGER
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
---
+++ win32_source/Settings/Interfaces.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2009 by Seth N. Hetu
+ *
+ * Please refer to the end of the file for licensing information
+ */
+
+#ifndef _INTERFACES
+#define _INTERFACES
+
+#include <string>
+#include <map>
+
+
+//Global "Types" enum
+enum TYPES {BUILTIN, IME_KEYBOARD, IME_ROMAN};
+
+
+//Simple class to manage our overriding options
+template <class T>
+class Option {
+public:
+	Option(T val=T())
+	{
+		this->value = val;
+		this->local = T();
+		this->user = T();
+		this->localSet = false;
+		this->userSet = false;
+	}
+	T get() const {
+		return userSet ? user : localSet ? local : value;
+	}
+	void setVal(T val) {
+		this->value = val;
+	}
+	void setLoc(T val) {
+		this->local = val;
+		this->localSet = true;
+	}
+	void setUsr(T val) {
+		this->user = val;
+		this->userSet = true;
+	}
+private:
+	T value;
+	T local;
+	bool localSet;
+	T user;
+	bool userSet;
+};
+
+//A struct... will refactor into separate classes later.
+struct Encoding {
+	Option<std::wstring> displayName;
+	Option<std::wstring> initial;
+	Option<std::wstring> imagePath;
+};
+
+
+//Expected interface: "Input Method"
+class InputMethod {
+public:
+	//Struct-like properties
+	Option<std::wstring> displayName;
+	Option<TYPES> type;
+
+	//Temp for now: just force this to be virtual
+	virtual void getString() = 0;
+};
+
+//Expected interface: "Input Method"
+class Transformation {
+public:
+	//Struct-like properties
+	Option<Encoding> fromEncoding;
+	Option<Encoding> toEncoding;
+	Option<TYPES> type;
+
+	//Temp for now: just force this to be virtual
+	virtual void convert() = 0;
+};
+
+//Expected interface: "Display Method"
+class DisplayMethod {
+public:
+	//Struct-like properties
+	Option<Encoding> encoding;
+	Option<TYPES> type;
+
+	//Temp for now: just force this to be virtual
+	virtual void typeText() = 0;
+};
+
+
+//Expected interface: "Factory"
+class Factory {
+public:
+	//Builders for each other interface
+	virtual InputMethod* makeInputMethod(std::wstring id, TYPES type, std::map<std::wstring, Option<std::wstring> > settings) = 0;
+	virtual DisplayMethod* makeDisplayMethod(std::wstring id, TYPES type, std::map<std::wstring, Option<std::wstring> > settings) = 0;
+	virtual Transformation* makeTransformation(std::wstring id, TYPES type, std::map<std::wstring, Option<std::wstring> > settings) = 0;
+};
+
+
+
+#endif //_INTERFACES
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
