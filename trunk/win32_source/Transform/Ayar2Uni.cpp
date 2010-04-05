5fae09cfd3ec3f9629ac49fc8fb8c06ca5c3efec
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2010-04-05 17:18:53 +0000

    Filled in Ayar2Uni since the approach is quite simple. Will have to debug VK_* commands later.

--- trunk/win32_source/Transform/Ayar2Uni.cpp
+++ trunk/win32_source/Transform/Ayar2Uni.cpp
@@ -11,10 +11,78 @@ Ayar2Uni::Ayar2Uni()
 }
 
 
+//Very general
+bool Ayar2Uni::IsConsonant(wchar_t letter) const
+{
+	if (letter>=L'\u1000' && letter<=L'\u102A')
+		return true;
+	if (letter==L'\u103F')
+		return true;
+	if (letter>=L'\u1040' && letter<=L'\u1049')
+		return true;
+	if (letter==L'\u104E')
+		return true;
+	return false;
+}
+
+
 //Convert
 void Ayar2Uni::convertInPlace(std::wstring& src) const
 {
-	//TODO
+	//Temporary algorithm: just split and move kinzi + tha-way-htoe + ya-yit
+	std::wstringstream res;
+	std::wstringstream currSyllable;
+	std::wstringstream currSyllablePrefix;
+	for (size_t i=0; i<src.length(); i++) {
+		//The next syllable starts at the first non-stacked non-killed consonant, or at tha-way-htoe or ya-yit
+		if (src[i]<L'\u1000' || src[i]>L'\u109F') {
+			//Append all non-Myanmar letters and continue
+			res <<currSyllablePrefix.str() <<currSyllable.str();
+			currSyllablePrefix.str(L"");
+			currSyllable.str(L"");
+			while (i<src.length() && (src[i]<L'\u1000' || src[i]>L'\u109F')) {
+				res <<src[i++];
+			}
+			i--;
+			continue;
+		}
+
+		//Are we at the boundary of a new word?
+		bool boundary = false;
+		if (src[i]==L'\u1031' || src[i]==L'\u103C')
+			boundary = true;
+		else if (IsConsonant(src[i])) {
+			boundary = true;
+			if (src[i]==L'\u1004' && i+2<src.length() && src[i+1]==L'\u103A' && src[i+2]==L'\u1039')
+				boundary = false; //Kinzi
+			if (i>0 && src[i-1]==L'\u1039')
+				boundary = false; //Stacked
+			else if (i+1<src.length() && src[i+1]==L'\u103A')
+				boundary = false; //Killed
+		}
+		if (boundary) {
+			res <<currSyllablePrefix.str() <<currSyllable.str();
+			currSyllablePrefix.str(L"");
+			currSyllable.str(L"");
+		}
+
+		//Now, collect as usual.
+		if (src[i]==L'\u1004' && i+2<src.length() && src[i+1]==L'\u103A' && src[i+2]==L'\u1039') {
+			res <<L"\u1004\u103A\u1039";
+			i+=2;
+		} else if (IsConsonant(src[i]) && currSyllablePrefix.str().empty()) //Only the first consonant is the prefix.
+			currSyllablePrefix <<src[i];
+		else
+			currSyllable <<src[i];
+
+		if (i==src.length()-1) {
+			res <<currSyllablePrefix.str() <<currSyllable.str();
+			currSyllablePrefix.str(L"");
+			currSyllable.str(L"");
+		}
+	}
+
+	src = res.str();
 }
 
 
