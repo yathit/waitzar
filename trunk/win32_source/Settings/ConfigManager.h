fe37e8a3011dffe581e850ef688dcc35b6ec3f06
seth.hetu <seth.hetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2011-02-02 18:48:34 +0000

    Removed comments for functionality that we've already replaced. "Last-used" and "override" settings remain....

--- trunk/win32_source/Settings/ConfigManager.h
+++ trunk/win32_source/Settings/ConfigManager.h
@@ -6,8 +6,6 @@
 
 #pragma once
 
-//#include "windows_wz.h"
-
 #include <vector>
 #include <set>
 #include <iostream>
@@ -18,7 +16,6 @@
 #include <stdexcept>
 
 #include "Json CPP/value.h"
-//#include "Json CPP/reader.h"
 
 #include "Settings/WZFactory.h"
 #include "Settings/ConfigTreeWalker.h"
@@ -46,139 +43,28 @@ public:
 		this->sealed = false;
 	}
 
+	//Means of building up a configuration, and then gettings its "Config" root node
+	void mergeInConfigFile(const std::string& cfgFile, const CfgPerm& perms, bool fileIsStream=false, std::function<void (const StringNode& n)> OnSetCallback=std::function<void (const StringNode& n)>(), std::function<void (const std::wstring& k)> OnError=std::function<void (const std::wstring& k)>());
+	const ConfigRoot& sealConfig(std::function<void (const std::wstring& k)> OnError=std::function<void (const std::wstring& k)>());
 
-	//Much better as static methods!
+	//Static helpers for loading/saving the "automated" config files.
 	static void SaveLocalConfigFile(const std::wstring& path, const std::map<std::wstring, std::wstring>& properties=std::map<std::wstring, std::wstring>());
 	static void SaveUserConfigFile(const std::wstring& path);
 
 
-	//Accessible by our outside class
-	/*const Settings& getSettings();
-	const std::set<Extension*>& getExtensions();
-	const std::set<Language>& getLanguages();
-	const std::set<InputMethod*>& getInputMethods();
-	const std::set<Encoding>& getEncodings();
-	const Transformation* getTransformation(const Language& lang, const Encoding& fromEnc, const Encoding& toEnc) const;
-	const std::set<DisplayMethod*>& getDisplayMethods();*/
-	//void overrideSetting(const std::wstring& settingName, bool value);
-
-	//Helpful
-	/*std::wstring getLocalConfigOpt(const std::wstring& key);
-	void clearLocalConfigOpt(const std::wstring& key);
-	void setLocalConfigOpt(const std::wstring& key, const std::wstring& val);
-	void saveLocalConfigFile(const std::wstring& path, bool emptyFile);
-	void saveUserConfigFile(const std::wstring& path, bool emptyFile);
-	bool localConfigCausedError();
-	void backupLocalConfigOpts();
-	void restoreLocalConfigOpts();*/
-
-	//Control
-	//Language activeLanguage;
-	//Encoding activeOutputEncoding;
-	//InputMethod* activeInputMethod;
-	//std::vector<DisplayMethod*> activeDisplayMethods; //Normal, small
-	//Encoding unicodeEncoding;
-
-	//Quality control
-	//void validate(HINSTANCE& hInst, MyWin32Window* mainWindow, MyWin32Window* sentenceWindow, MyWin32Window* helpWindow, MyWin32Window* memoryWindow, OnscreenKeyboard* helpKeyboard, const std::map<std::wstring, std::vector<std::wstring>>& lastUsedSettings);
-
-	//Useful
-	//bool IsProbablyFile(const std::wstring& str);
-	//std::wstring purge_filename(const std::wstring& str);
-	//std::wstring sanitize_id(const std::wstring& str);
-	//std::wstring sanitize_value(const std::wstring& str, const std::wstring& filePath);
-	//std::vector<std::wstring> separate(std::wstring str, wchar_t delim);
-	//bool read_bool(const std::wstring& str);
-	//int read_int(const std::wstring& str);
-
-	//Kind of out of place, but it works
-	//void generateHotkeyValues(const wstring& srcStr, HotkeyData& hkData);
-
-
-public:
-	void mergeInConfigFile(const std::string& cfgFile, const CfgPerm& perms, bool fileIsStream=false, std::function<void (const StringNode& n)> OnSetCallback=std::function<void (const StringNode& n)>(), std::function<void (const std::wstring& k)> OnError=std::function<void (const std::wstring& k)>());
-	const ConfigRoot& sealConfig(std::function<void (const std::wstring& k)> OnError=std::function<void (const std::wstring& k)>());
-
 private:
-	//These two functions replace "readInConfig"
+	//Internal methods used for parsing a javascript tree and merging it into the config tree piece-wise.
+	//  The first method calls the second two, and manages error messages automatically.
 	void buildAndWalkConfigTree(const JsonFile& file, StringNode& rootNode, GhostNode& rootTNode, const TransformNode& rootVerifyNode, const CfgPerm& perm, std::function<void (const StringNode& n)> OnSetCallback, std::function<void (const std::wstring& k)> OnError);
 	void buildUpConfigTree(const Json::Value& root, StringNode& currNode, const std::wstring& currDirPath, std::function<void (const StringNode& n)> OnSetCallback);
 	void walkConfigTree(StringNode& source, GhostNode& dest, const TransformNode& verify, const CfgPerm& perm);
-	//void buildVerifyTree();
-	//std::map<std::wstring, std::wstring> locallySetOptions; //TODO: Replace later
+
+	//Internal storage data.
 	StringNode root;
 	ConfigRoot troot;
-	//TransformNode verifyTree;
 	bool sealed;
 
-
-
-	//void readInConfig(const Json::Value& root, const std::wstring& folderPath, std::vector<std::wstring> &context, bool restricted, bool allowDLL, std::map<std::wstring, std::wstring>* const optionsSet);
-	//void setSingleOption(const std::wstring& folderPath, const std::vector<std::wstring>& name, const std::wstring& value, bool restricted, bool allowDLL);
-
-	//void resolvePartialSettings();
-	//void generateInputsDisplaysOutputs(const std::map<std::wstring, std::vector<std::wstring> >& lastUsedSettings);
-
-//private:
-	//Our many config files.
-	/*JsonFile mainConfig;
-	JsonFile commonConfig;
-	std::map<JsonFile , std::vector<JsonFile> > langConfigs;
-	JsonFile localConfig;
-	JsonFile userConfig;*/
-
-	//Workaround
-	//std::string (*getMD5Function)(const std::string&);
-
-
-	//Current working directory
-	//std::wstring workingDir;
-
-	//Have we loaded...?
-	/*bool loadedSettings;
-	bool loadedLanguageMainFiles;
-	bool loadedLanguageSubFiles;*/
-
-	//Functions for loading some of these.
-	//void loadLanguageMainFiles();
-	//void loadLanguageSubFiles();
-
-	//Temporary option caches for constructing complex structures
-	//Will eventually be converted into real InputManager*, etc.
-	//Store as <lang_name,item_name>, for fast lookup.
-	/*std::map<std::pair<std::wstring,std::wstring>, std::map<std::wstring, std::wstring> > partialInputMethods;
-	std::map<std::pair<std::wstring,std::wstring>, std::map<std::wstring, std::wstring> > partialEncodings;
-	std::map<std::pair<std::wstring,std::wstring>, std::map<std::wstring, std::wstring> > partialTransformations;
-	std::map<std::pair<std::wstring,std::wstring>, std::map<std::wstring, std::wstring> > partialDisplayMethods;*/
-
-	//The actual representation
-	//OptionTree options;
-	//std::map<std::wstring, std::wstring> localOpts;
-	//std::map<std::wstring, std::wstring> localOptsBackup;
-	//bool localConfError;
-
-	//Cache
-	//Transformation* self2self;
 };
-
-
-
-//Find and return an item in a Set, indxed only by its wstring
-//   * This is not needed for value-type sets, since we can just construct temporaries
-//   * We don't need a non-const version, because set iterators are always const
-/*template <class T>
-typename std::set<T*>::const_iterator FindKeyInSet(const std::set<T*>& container, const std::wstring& key)
-{
-	typename std::set<T*>::const_iterator it=container.begin();
-	for (; it!=container.end(); it++)  {
-		if ((*(*it)) == key)
-			break;
-	}
-	return it;
-}*/
-
-
-
 
 
 /*
