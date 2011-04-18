adf978fef099e42b0db02c01ad74719d03666c50
sethhetu <sethhetu@f96070ac-a43e-0410-952c-47c7f09c98be>
2011-04-18 07:23:03 +0000

    Basic normalization works. Need to test with all characters, complex (kinzi, etc.) letters, sentences, etc.

--- shan_convert_web/convert.js
+++ shan_convert_web/convert.js
@@ -28,7 +28,8 @@ var dirConvert = {
   '\u0050' : '\u1081\u1082\u103A',
   '\u0051' : '\u1022',
   '\u0052' : '\u103B\u103D',  
-  '\u0053' : '\u1004\u103A\u1039',  
+  //'\u0053' : '\u1004\u103A\u1039',
+  '\u0053' : '\uAA7F', //NOTE: This is a temp. hack, since kinzi is the only multi-code-point letter here
   '\u0054' : '\u103C',
   '\u0055' : '\u1075',
   '\u0056' : '\uAA6E',
@@ -143,12 +144,132 @@ var dirConvert = {
   //Whitespace
   '\t' : '\t',
   '\n' : '\n',
-  '\u0020' : '\u0020',
-  
-
-  
-  'null' : 'null'
+  '\u0020' : '\u0020'
 };
+
+
+//Helper
+var CONS =   
+  '\u1000\u1001\u1002\u1003\u1004\u1005\u1006\u1007\u1008\u1009\u100A\u100B\u100C\u100D\u100E\u100F'
+ +'\u1010\u1011\u1012\u1013\u1014\u1015\u1016\u1017\u1018\u1019\u101A\u101B\u101C\u101D\u101E\u101F'
+ +'\u1020\u1021\u1022\u1023\u1024\u1025\u1026\u1027\u1028\u1029\u102A'
+ +'\u103F'
+ +'\u1041\u1042\u1043\u1044\u1045\u1046\u1047\u1048\u1049'
+ +'\u104E'
+ +'\u105A\u105B\u105C\u105D'
+ +'\u1061\u1065\u1066'
+ +'\u106E\u106F\u1070'
+ +'\u1075\u1076\u1077\u1078\u1079\u107A\u107B\u107C\u107D\u107E\u107F\u1080\u1081'
+ +'\u108E'
+ +'\uAA60\uAA61\uAA62\uAA63\uAA64\uAA65\uAA66\uAA67\uAA68\uAA69\uAA6A\uAA6B\uAA6C\uAA6D\uAA6E\uAA6F'
+ +'\uAA71\uAA72\uAA73\uAA74\uAA75\uAA76';
+
+
+//Input normalization order. Cannot go backwards
+var input_norm = [
+  //E vowel
+  '\u1031\u1084',
+
+  //Medial R
+  '\u103C',
+
+  //Consonant
+  CONS,
+
+  //Everything else
+  '\uAA7F'
+ +'\u103B\u105E\u105F'
+ +'\u103D\u1082'
+ +'\u103E\u1060'
+ +'\u102D\u102E\u1032\u1033\u1034\u1035\u1036\u1071\u1072\u1073\u1074\u1085\u109D'
+ +'\u102F\u1030'
+ +'\u1086'
+ +'\u102B\u102C\u1062\u1063\u1067\u1068\u1083'
+ +'\u1036\u1032'
+ +'\u1037'
+ +'\u103A'
+ +'\u1038\u1087\u1088\u1089\u108A\u108B\u108C\u108D\u108F\u109A\u109B\u109C'
+];
+
+//Output normalization order
+var output_norm = [
+  //Kinzi
+  '\uAA7F',
+
+  //Consonant
+  CONS,
+
+  //(No stacked1, stacked2, asat)
+
+  //Medial Y, R, W, H
+  '\u103B\u105E\u105F',
+  '\u+103C',
+  '\u103D\u1082',
+  '\u103E\u1060',
+
+  //(No mon asat)
+
+  //E vowel
+  '\u1031\u1084',
+
+  //(No shan E vowel)
+
+  //Upper vowel, lower vowel
+  '\u102D\u102E\u1032\u1033\u1034\u1035\u1036\u1071\u1072\u1073\u1074\u1085\u109D',
+  '\u102F\u1030',
+
+  //(No Karen vowel)
+
+  //Shan vowel
+  '\u1086',
+
+  //A vowel
+  '\u102B\u102C\u1062\u1063\u1067\u1068\u1083',
+
+  //Anusvara
+  '\u1036\u1032',
+
+  //(No Pwo tone)
+
+  //Dot below
+  '\u1037',
+
+  //(No mon H)
+
+  //Visible virama
+  '\u103A',
+
+  //Visarga
+  '\u1038\u1087\u1088\u1089\u108A\u108B\u108C\u108D\u108F\u109A\u109B\u109C'
+  
+  //(No reduplication)
+];
+
+//Match one of these arrays, return an id:
+function match_arr(arr, letter) {
+  for (id in arr) {
+    var entry = arr[id];
+    if (entry.search(letter) != -1) {
+      return id;
+    }
+  }
+  return -1;
+}
+
+
+//Flush an array of strings
+function flush(arr) {
+  var res = '';
+  for (id in arr) {
+    var elem = arr[id];
+    if (elem=='\uAA7F') {
+      elem =  '\u1004\u103A\u1039';
+    }
+    res += elem;
+    arr[id] = '';
+  }
+  return res;
+}
 
 
 
@@ -166,7 +287,59 @@ function convert(source) {
     }
   }
 
-  return res;   
+  //Now, normalize it
+  var final_result = '';
+  var norm_in_id = 0;
+  var norm_out = [];
+  norm_out.length = output_norm.length;
+  for (var i=0; i<norm_out.length; i++) {
+    norm_out[i] = '';
+  }
+  for (c in res) {
+    var letter = res[c];
+    
+    //Get its corresponding entry IDs in input_norm and output_norm
+    var in_id = match_arr(input_norm, letter);
+    var out_id = match_arr(output_norm, letter);    
+    if (in_id==-1 && out_id==-1) {
+      //No match; flush the input
+      final_result += flush(norm_out);
+      norm_in_id = 0;
+
+      //Append it
+      final_result += letter;
+
+      //Report unknown unicode letters
+      var ord = letter.charCodeAt[0];
+      if ((ord>=0x1000 && ord<=0x109F) || (ord>=0xAA60 && ord<=0xAA7F)) {
+        unknown += (unknown.length!=0?' ':' ') + '(' + ord + ')';
+      }
+    } else if (in_id==-1 || out_id==-1) {
+      //Error!
+      unknown = 'Error: input and output conflict for: ' + letter;
+      return '';
+    } else {
+      //A match exists; add it to output_norm. But first, check if this will make us 'go backwards'
+      if (in_id < norm_in_id) {
+        final_result += flush(norm_out);
+        norm_in_id = 0;
+      }
+      
+      //Slot it
+      norm_out[out_id] += letter;
+      norm_in_id = in_id;
+
+      //Special case! Consonant will always advance the ID by 1:
+      if (norm_in_id==2) {
+        norm_in_id += 1;
+      }
+    }
+  }
+
+  //Append any remaining letters
+  final_result += flush(norm_out);
+
+  return final_result;
 }
 
 
